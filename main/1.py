396149e7ab6426fe2733e785b3857f4e33b9d1f2
Chammi22 <osenyayaromashka@gmail.com>
2020-08-20 17:26:49 +0300

    improved function

--- main/1.py
+++ main/1.py
@@ -1,4 +1,4 @@
-def ispolindrome(str):
+def is_polindrome(str):
     for i in range(0, int(len(str) / 2)):
         if str[i] != str[len(str) - i - 1]:
             return False
@@ -6,41 +6,44 @@ def ispolindrome(str):
 
 
 str = "romamor"
-print(ispolindrome(str))
+print(is_polindrome(str))
 
 
-def Polindrome(string):
+def polindrome(string):
     if string == string[::-1]:
         return True
     return False
 
 
-print(Polindrome("abccba"))
+print(polindrome("abccba"))
 
 
-def getdigit(number):
-    Tup = [int(i) for i in str(number)]
-    print(Tup)
+def get_digit(number):
+    tuple = [int(i) for i in str(number)]
+    print(tuple)
 
 
 number = 1233312
-getdigit(number)
+get_digit(number)
 
 
-def get_longest_word(str):
-    str = str.split()
-    print(str)
-    word_index = 0
-    word_length = 0
-    count = -1
-    for i in str:
-        count += 1
-        if len(i) > word_length:
-            word_length = len(i)
-            word_index = count
-    print(str[word_index])
+def length(element):
+    return len(element)
 
 
+def get_longest_word(string):
+    string = string.split()
+    string.sort(reverse=True, key=length)
+    print(string[0])
+
+
+def get_max_word(string):
+    string = string.split()
+    print(max(string, key=length))
+
+
+#
+#
 get_longest_word("Python is simple and effective")
 
 
